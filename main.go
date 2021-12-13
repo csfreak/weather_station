e0b0ccb675cc336a39a8251b970636a08d40f65f
Seth Miles <miles.seth@gmail.com>
2021-12-12 19:19:44 -0600

    Clean some logging and printing, add some ease of life decoupling (#1)

--- main.go
+++ main.go
@@ -2,27 +2,30 @@ package main
 
 import (
 	"fmt"
-	"net/http"
-
 	"github.com/csfreak/weather_station/weather"
+	"log"
+	"net/http"
 )
 
+var apiListenerPort = ":8080"
+
 func main() {
-	fmt.Println("Staring main func")
 	http.HandleFunc("/v1/ecowitt", ecowittHandler)
 	http.HandleFunc("/v1/ecowitt/", ecowittHandler)
 
-	fmt.Println("Starting HTTP Server")
-	err := http.ListenAndServe(":8080", nil)
-	fmt.Println(err)
-	fmt.Println("After HTTP Server")
+	fmt.Println("Starting HTTP Server on", apiListenerPort)
+	log.Fatal(http.ListenAndServe(apiListenerPort, nil))
 }
 
 func ecowittHandler(res http.ResponseWriter, req *http.Request) {
 	if !(req.Method == http.MethodPost) {
 		res.WriteHeader(http.StatusMethodNotAllowed)
 	} else {
-		req.ParseForm()
+		parseErr := req.ParseForm()
+		if parseErr != nil {
+			fmt.Println(parseErr)
+			return
+		}
 		fmt.Println(req.PostForm)
 		w, err := weather.FromEcowitt(req.PostForm)
 		if err != nil {
